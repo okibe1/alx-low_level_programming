a9cac3ee00060c908f1e32b2838618591f639a1c
okibe1 <okibe4obinna@gmail.com>
2022-10-13 05:36:51 -0700

    Corrected task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,11 @@
 
 
 /**
+ *   main - Entry point  
  *
- *  * main - Entry point
+ *     Return: Always 0 (Success)
  *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ */
 
 int main(void)
 
@@ -18,6 +15,6 @@ int main(void)
 
 		puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+		return (0);
 
 }
