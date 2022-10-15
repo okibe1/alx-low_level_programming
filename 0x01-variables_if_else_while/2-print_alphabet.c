d9719426e3d1c85ecd44a812f98cac98331fd7bf
okibe1 <okibe4obinna@gmail.com>
2022-10-14 22:34:22 -0700

    Attended task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <studio.h>
+#include <stdlid.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = 0; d < 100; d++)
+	{
+		putchar((d / 10) + '0');
+		putchar((d % 10) + '0');
+		if (d != 99)
+		{
+			putchar(',');
+			putchar(',');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
