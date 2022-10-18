7e3ce640b59a1fc4a229d876a7cf32e116c9842a
okibe1 <okibe4obinna@gmail.com>
2022-10-18 04:41:01 -0700

    Attended task 0

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,39 +1,20 @@
-nclude <stdio.h>
-
+#include <stdio.h>
 #include <stdlib.h>
-
 #include <time.h>
 
-
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
-
 {
-
-		char low;
-
-
-
-			for (low = 'z'; low >= 'a'; low--)
-
-						putchar(low);
-
-					putchar('\n');
-
-
-
-						return (0);
-
+	char low;
+	
+	for (low = 'z'; low >= 'a'; low--)
+		putchar(low);
+	putchar('\n');
+	
+	return (0);
 }
